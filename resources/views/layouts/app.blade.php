5ea52d05a9ca57c016c0872b348e1080aa5b853b
Jaison M <jaison.mora@mcets-inc.com>
2023-10-19 11:20:59 -0500

    base de diseño

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,17 +15,9 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gradient-to-r from-slate-800 to-slate-900">
+            <div class="h-2 bg-gradient-to-r from-slate-900 to-slate-800"></div>
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
             <!-- Page Content -->
             <main>
