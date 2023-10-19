835801d00682c007a5d43998ad7deeab3135cbe8
Jaison M <jaison.mora@mcets-inc.com>
2023-10-19 13:53:47 -0500

    generacion de categorias con el uso de livewire en un componente

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- style -->
-        @livewireStyles
+        {{-- @livewireStyles --}}
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -27,7 +27,7 @@
                 {{ $slot }}
             </main>
         </div>
-        
-        @livewireScripts
+
+        {{-- @livewireScripts --}}
     </body>
 </html>
