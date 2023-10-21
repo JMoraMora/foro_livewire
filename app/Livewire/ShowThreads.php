2778929c00752e1f28005d4c986bb3789f95d089
Jaison Mora <jasonjairmoramora@gmail.com>
2023-10-21 00:04:00 -0500

    filtrado por busqueda de threads y category

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -10,7 +10,13 @@ use Livewire\Component;
 
 class ShowThreads extends Component
 {
-    public $search = 'abc123';
+    public $search = '';
+    public $category = '';
+
+    public function filterByCategory($category)
+    {
+        $this->category = $category;
+    }
     
     public function render()
     {
@@ -18,12 +24,17 @@ class ShowThreads extends Component
 
         $threads = Thread::query();
         $threads->where('title', 'like', "%$this->search%");
+
+        if(!empty($this->category)) {
+            $threads->where('category_id', $this->category);
+        }
+
         $threads->withCount('replies');
         $threads->latest();
 
         return view('livewire.show-threads', [
             'categories' => $categories,
-            'threads' => $threads->get(),
+            'threads' => $threads->get()
         ]);
     }
 }
