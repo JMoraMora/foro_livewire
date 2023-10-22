8bec007a0ac47964b8cba2eefbae47be0a63cf42
Jaison Mora <jasonjairmoramora@gmail.com>
2023-10-22 11:31:31 -0500

    listado de respuestas hijas

--- app/Livewire/ShowThread.php
+++ app/Livewire/ShowThread.php
@@ -27,7 +27,7 @@ class ShowThread extends Component
     public function render()
     {
         return view('livewire.show-thread', [
-            'replies' => $this->thread->replies()->get()
+            'replies' => $this->thread->replies()->whereNull('reply_id')->get()
         ]);
     }
 }
